0c4f968ca56b16e131fe373ef888e9a8f1fc5e29
Raunak-1607 <raunakbiswas537@gmail.com>
2026-09-21 22:35:15 +0600

    Updated The Fetch Path

--- src/app/Books/page.tsx
+++ src/app/Books/page.tsx
@@ -4,7 +4,7 @@ import { IBook } from '../Type/Type';
 
 const getBooks = async () => {
     try {
-        const res = await fetch("http://localhost:3000/booksData.json");
+        const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
         const data = await res.json();
         return data;
     }
